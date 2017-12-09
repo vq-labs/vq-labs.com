6d0d9a53e961daa8e89aacc379baf72f7a1c28c2
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-12-09 13:06:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# What is it?
+Source files for the VQ-Labs web page vq-labs.com / vq-labs.de / viciqloud.com.
+
+# What is used?
+bootstrap 3, npm, gulp with html/css/js/json/img minify + uglify
+
+# How to use it?
+All the html sources can be found in the "src" folder. Modify the files in there and run "gulp build" in the root folder to get all the dependencies and minified versions of your CSS/JS/HTML files generated for you.
+Images, css and other files that do not require pre-processing you will find in the "public" folder.
+
+# How to start?
+```
+git clone https://github.com/adrianbarwicki/vq-labs.com 
+
+cd vq-labs.com
+
+npm install
+
+npm start
+```
+# Development
+This will listen for changes under the src/ and will build automatically:
+```
+gulp watch
+```
+
+You can then start a server with for example:
+```
+live-server ./public
+```
+
+@todo: to make the above two in one command
+
+# Deployment
+Make sure that you have s3-deploy installed globally:
+```
+npm install s3-deploy --g
+```
+
+The following command will prepare, build and deploy the app to S3 bucket:
+```
+gulp deploy
+```
+
+# License?
+Apache 2.0
---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const replace = require('gulp-replace-task');
+const spawn = require('child_process').spawn;
+const fileinclude = require('gulp-file-include');
+const liveServer = require('gulp-live-server');
+const runSequence = require('run-sequence');
+
+const build = (VQ_TENANT_API_URL, env) => {
+    gulp.src([ 'src/**/index.html' ])
+    .pipe(replace({
+        patterns: [
+            {
+                match: 'VQ_TENANT_API_URL',
+                replacement: VQ_TENANT_API_URL
+            },
+            {
+                match: 'VQ_WEB_ENV',
+                replacement: env
+            }
+        ]
+    }))
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('public'));
+
+    gulp.src([ 'src/**/*.js' ])
+    .pipe(replace({
+        patterns: [
+            {
+                match: 'VQ_TENANT_API_URL',
+                replacement: VQ_TENANT_API_URL
+            },
+            {
+                match: 'VQ_WEB_ENV',
+                replacement: env
+            }
+        ]
+    }))
+    .pipe(gulp.dest('public'));
+
+  gulp.src([ 'src/**/*.css' ])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('public'))
+
+    gulp.src([ 'assets/**/*' ])
+    .pipe(gulp.dest('public'))
+};
+
+gulp.task('run', function(cb) {
+    runSequence(
+        'build:local',
+        'watch:local',
+        'runServer',
+        cb
+    );
+});
+
+gulp.task('runServer', function() {
+    var server = liveServer.static('./public');
+    server.start();
+});
+
+// production
+gulp.task('build', () => build('https://vqmarketplace.vq-labs.com/api', 'production'));
+
+gulp.task('build:dev', () => build('https://vqmarketplace.vqmarketplace.com/api', 'development'));
+
+gulp.task('build:local', () => build('http://localhost:8081/api', 'local'));
+
+gulp.task('deploy', [ 'build' ], function() {
+    const args = [ './**', '--region', 'eu-central-1', '--bucket', 'vq-labs.com', '--gzip' ];
+    const npm = spawn("s3-deploy", args, { cwd: './public' });
+
+    npm.stdout.on('data', data => {
+        console.log(`stdout: ${data}`);
+    });
+
+    npm.stderr.on('data', data => {
+        console.log(`stderr: ${data}`);
+    });
+
+    npm.on('close', code => {
+        console.log(code !== 0 ? 'error in build' : 0);
+    });
+});
+
+gulp.task('watch', () => gulp.watch('./src/**/**',  [ 'build' ]));
+gulp.task('watch:dev', () => gulp.watch('./src/**/**',  [ 'build:dev' ]));
+gulp.task('watch:local', () => gulp.watch('./src/**/**',  [ 'build:local' ]));
